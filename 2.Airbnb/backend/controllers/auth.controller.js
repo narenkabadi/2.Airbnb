a5929133464209a7201a4eeecb2056ec0782c877
narenkabadi <152527301+narenkabadi@users.noreply.github.com>
2025-10-08 15:59:14 +0530

    Update auth.controller.js

--- 2.Airbnb/backend/controllers/auth.controller.js
+++ 2.Airbnb/backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const sighUp=async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
@@ -42,7 +42,7 @@ export const login = async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
